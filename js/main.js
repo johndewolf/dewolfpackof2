6a43b84eb44489f4780af7277138dcbf6d002a43
John DeWolf <jdewolf@backupify.com>
2016-01-03 19:24:52 -0500

    update for pics on mobile

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ $( document ).ready(function() {
 	$(".weddingPartyMember").on("click", function() {
 		var party = $(this).parents().eq(5)[0];
 		var active = party.getElementsByClassName("active");
-		
 
 		$("#" + party.id + "Feature").empty();
 		$("#" + party.id + "Feature").append("<img src='img/"+$(this).parent()[0].id+".jpg' />");
@@ -11,7 +10,6 @@ $( document ).ready(function() {
 		for (var x = 0; x < active.length + 1; x++) {
 			$(active[0]).removeClass("active");
 		}
-
 		$(this).addClass("active");
 		$(this).siblings().addClass("active");
 
