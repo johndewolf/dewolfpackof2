9a1c1ac75a3f9eed8db05716d3a8c6fe2a4764dc
johndewolf <jdewolf06@gmail.com>
2016-01-29 13:00:47 -0500

    fade in and out for images

--- js/main.js
+++ js/main.js
@@ -3,9 +3,14 @@ $( document ).ready(function() {
 	$(".weddingPartyMember").on("click", function() {
 		var party = $(this).parents().eq(5)[0];
 		var active = party.getElementsByClassName("active");
+		var featImage = $("#" + party.id + "Feature").children()[0]
+		var imageParent = this;
+		$(featImage).fadeOut("fast", function() {
+			$("#" + party.id + "Feature").empty();
+			$("#" + party.id + "Feature").append("<img src='img/"+$(imageParent).parent()[0].id+".jpg' />");
+		});
 
-		$("#" + party.id + "Feature").empty();
-		$("#" + party.id + "Feature").append("<img src='img/"+$(this).parent()[0].id+".jpg' />");
+
 
 		for (var x = 0; x < active.length + 1; x++) {
 			$(active[0]).removeClass("active");
